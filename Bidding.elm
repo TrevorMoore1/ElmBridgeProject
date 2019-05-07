66f56af53e9ce748d5109b488fee33a0bb2f04a6
Raphael Hallerman <rhallerman@cs.uchicago.edu>
2019-05-06 23:55:02 -0500

    updated with next steps in README

--- Bidding.elm
+++ Bidding.elm
@@ -1,56 +1,90 @@
+module Bidding exposing (..)
+
 type alias Bid = (Int, Suit)
 type Suit = Spade | Heart | Diamond | Club | NoTrump | Pass
-type alias Points = Int
-type alias Shape = {spades   : Int,
-                    hearts   : Int,
-                    diamonds : Int,
-                    clubs    : Int}
-type alias HandType = (Shape, Points)
-type alias BidDefinition = {requirements   : List HandType,
+type alias HandRange = {spades   : (Int,Int),
+                        hearts   : (Int,Int),
+                        diamonds : (Int,Int),
+                        clubs    : (Int,Int),
+                        points   : (Int,Int)}
+type alias HandType = { spades   :   Int,
+                        hearts   :   Int,
+                        diamonds :   Int,
+                        clubs    :   Int,
+                        points   :   Int}
+type BidDefinition = BidDefinition
+                            {requirements   : List HandRange,
                             bidValue       : Bid, 
-                            subsequentBids : BiddingRules}
+                            subsequentBids : List BidDefinition}
 type alias BiddingRules = List BidDefinition
 type alias BidSequence = List Bid
 
-defineBid : BiddingRules -> BidSequence -> List HandType -> Maybe BiddingRules
-defineBid system previousBids requirements =
-    case previousBids of
-        []               -> Nothing
-        lastBid::[]      -> Just ({requirements = }::system)
-        firstBid::(rest) -> case findBid system firstBid of
-            Just subsequentBids -> defineBid subsequentBids rest requirements bid
-            Nothing -> Nothing
-                
 
+
+-- Input: an entire system, a history (ending in bid that we're defining), applicable hand types
+-- Output: a new bidding rule
+defineBid : BiddingRules -> BidSequence -> List HandRange -> Maybe BiddingRules
+defineBid system history bidMeaning =
+    case history of
+        []               -> Nothing
+        lastBid::[]      -> Just ((BidDefinition{requirements = bidMeaning, bidValue = lastBid, subsequentBids = []})::system)
+        firstBid::rest -> 
+            case getNextBids system firstBid of
+                Just subsequentBids -> defineBid subsequentBids rest bidMeaning
+                Nothing -> Nothing
+                
+-- Input: a system, a history (with last bid being the one we want to prioritize)
+-- Output: a system
 prioritizeBid : BiddingRules -> BidSequence -> BiddingRules
 prioritizeBid system previousBids =
+    Debug.todo "TODO"
 
+-- Input: a system, a history (with last bid being the one we want to remove)
+-- Output: the system, minus the last bid
 removeBid : BiddingRules -> BidSequence -> BiddingRules
+removeBid system history =
+    Debug.todo "TODO"
 
 
+-- Input: a system, current handtype
+-- Output: makes a bid if possible
 makeBid : BiddingRules -> HandType -> Maybe (Bid, BiddingRules)
 makeBid nextBids hand =
     case nextBids of
         []                  -> Nothing
         priorityBid::rest   ->  if correctBid priorityBid hand 
-                                then Just (priorityBid.bidValue, priorityBid.subsequentBids)
+                                then 
+                                    let (BidDefinition topChoice) = priorityBid in
+                                    Just (topChoice.bidValue, topChoice.subsequentBids)
                                 else makeBid rest hand
             
-
-
+-- Input: a potential bid, the current hand
+-- Output: whether the bid is applicable
 correctBid : BidDefinition -> HandType -> Bool
-correctBid bid hand = 
-    case bid.requirements of
-    []             -> False
-    handType::rest ->   if handType == hand 
-                        then True
-                        else correctBid (rest, bid.bidValue, bid.subsequentBids) hand
+correctBid (BidDefinition bid) hand = 
+    List.foldr (||) False (List.map (handInRange hand) bid.requirements)
 
-findBid : BiddingRules -> Bid -> Maybe BiddingRules
-findBid bidList bid =
+-- Input: a system, a bid
+-- Output: 
+getNextBids : BiddingRules -> Bid -> Maybe BiddingRules
+getNextBids bidList bid =
     case bidList of
         [] -> Nothing
-        priorityBid::rest ->    if bid.bidValue == prioirtyBid.bidValue
+        (BidDefinition priorityBid)::rest ->    if bid == priorityBid.bidValue
                                 then Just priorityBid.subsequentBids
-                                else findBid rest bid
-            
+                                else getNextBids rest bid
+
+-- Input: current hand, candidate range
+-- Output: whether the hand fits the range
+handInRange : HandType -> HandRange -> Bool
+handInRange hand range =
+       hand.spades >= Tuple.first(range.spades)
+    && hand.spades <= Tuple.second(range.spades)
+    && hand.spades >= Tuple.first(range.hearts)
+    && hand.spades <= Tuple.second(range.hearts)
+    && hand.spades >= Tuple.first(range.diamonds)
+    && hand.spades <= Tuple.second(range.diamonds)
+    && hand.spades >= Tuple.first(range.clubs)
+    && hand.spades <= Tuple.second(range.clubs)
+    && hand.spades >= Tuple.first(range.points)
+    && hand.spades <= Tuple.second(range.points)
