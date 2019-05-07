8d8714ad278268e132a40da38c3f0bf58d539487
Raphael Hallerman <rhallerman@cs.uchicago.edu>
2019-05-06 21:59:39 -0500

    1 change

--- Bidding.elm
+++ Bidding.elm
@@ -1,4 +1,5 @@
-type alias Bid = (Int, Suit) 
+type alias Bid = (Int, Suit)
+// Raphael doesn't know how to code
 type Suit = Spade | Heart | Diamond | Club
 type alias Points = Int
 type alias Shape = {spades   : Int,
